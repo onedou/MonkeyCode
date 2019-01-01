206d10eceab26163ad23ef3b33e6db46ad4beb8c
onedou <onedou@126.com>
2019-01-01 22:02:45 +0800

    add template

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core'
+import { WOW } from 'wowjs'
+
+declare const $
 
 @Component({
   selector: 'index',
-  templateUrl: './index.component.html'
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.scss']
 })
 export class IndexComponent {
   title = 'MonkeyCode'
+
+  constructor() {
+    new WOW().init()
+  }
+
+  ngAfterViewInit() {
+    $('.flexslider').flexslider({
+      animation: 'slide',
+      start: function(slider) {
+        $('body').removeClass('loading')
+      }
+    })
+  }
 }
