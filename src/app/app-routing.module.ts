895f755bf228d99efd4da7daba01dc9afe09c2a6
tpp <tpp@tpps-Mac-mini.lan>
2019-03-12 10:29:23 +0800

    add sign-up component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { IndexComponent } from './components/index/index.component'
 import { IndexModule } from './components/index/index.module'
+import { signUpComponent } from './components/signUp/signUp.component'
+import { signUpModule } from './components/signUp/signUp.module'
 
 const routes: Routes = [
-  {path: '', component: IndexComponent}
+  {path: '', component: IndexComponent},
+  {path: 'signUp', component: signUpComponent}
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    IndexModule
+    IndexModule,
+    signUpModule
   ],
   exports: [RouterModule]
 })
---
+++ src/app/components/signUp/signUp.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'signUp',
+  templateUrl: './signUp.component.html',
+  styleUrls: ['./signUp.component.scss']
+})
+export class signUpComponent {}
