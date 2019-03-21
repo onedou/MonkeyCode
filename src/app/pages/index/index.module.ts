dd08a8d59348bc5dfc008d8af18c6bda1db0e2c6
onedou <onedou@126.com>
2019-03-21 10:09:01 +0800

    add material/animations/cdk

--- src/app/pages/index/index.module.ts
+++ src/app/pages/index/index.module.ts
@@ -1,16 +1,22 @@
 import { IndexComponent } from './index.component'
 import { NgModule } from '@angular/core'
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule
+} from '@angular/material'
 
 import { FooterModule } from '../../components/footer/footer.module'
 import { HeaderModule } from '../../components/header/header.module'
 
 @NgModule({
-  declarations: [
-    IndexComponent
-  ],
+  declarations: [IndexComponent],
   imports: [
     HeaderModule,
-    FooterModule
+    FooterModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule
   ]
 })
 export class IndexModule {}
