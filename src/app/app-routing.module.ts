40b2a13b73aede035e8d7080bca8bd815aeeee0e
tpp <tpp@tpps-Mac-mini.local>
2019-06-04 17:32:28 +0800

    Mobile version of web page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,15 @@ import { SignUpComponent } from './pages/signUp/signUp.component'
 import { SignUpModule } from './pages/signUp/signUp.module'
 import { CurriculumComponent } from './pages/curriculum/curriculum.component'
 import { CurriculumModule } from './pages/curriculum/curriculum.module'
+import { ContactComponent } from './pages/contact/contact.component'
+import { ContactModule } from './pages/contact/contact.module'
 
 
 const routes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'signUp', component: SignUpComponent},
-  {path: 'curriculum', component: CurriculumComponent}
+  {path: 'curriculum', component: CurriculumComponent},
+  {path: 'contact', component: ContactComponent}
 ];
 
 @NgModule({
@@ -20,7 +23,8 @@ const routes: Routes = [
     RouterModule.forRoot(routes),
     IndexModule,
     SignUpModule,
-    CurriculumModule
+    CurriculumModule,
+    ContactModule
   ],
   exports: [RouterModule]
 })
---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+declare const $
+
+@Component({
+  selector: 'contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent {
+  constructor() {}
+}
