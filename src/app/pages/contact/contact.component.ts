85e76921ad2484c108f34d8f4283f7a1d57cb837
tpp <tpp@tpps-Mac-mini.local>
2019-06-13 17:33:47 +0800

    update Bmap

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -12,8 +12,10 @@ export class ContactComponent implements OnInit {
 
   ngOnInit() {
     const map = new BMap.Map('map');//创建地图实例
-    const point = new BMap.Point(116.404, 39.915);//创建点坐标
+    const point = new BMap.Point(113.088134,22.96479);//创建点坐标
     map.centerAndZoom(point, 15);//初始化地图，设置中心点坐标和地图级别
     map.enableScrollWheelZoom(true);//开启鼠标滚轮缩
+    var marker = new BMap.Marker(point);        // 创建标注    
+    map.addOverlay(marker);
   }
 }
