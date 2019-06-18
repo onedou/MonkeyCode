b51ffc0851f6d6a08caf0128c275f2895ceb1cc2
tpp <tpp@tpps-Mac-mini.local>
2019-06-18 18:09:01 +0800

    alignment update

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -15,6 +15,10 @@ export class ContactComponent implements OnInit {
     const point = new BMap.Point(113.088134,22.96479);//创建点坐标
     map.centerAndZoom(point, 15);//初始化地图，设置中心点坐标和地图级别
     map.enableScrollWheelZoom(true);//开启鼠标滚轮缩
+    map.addControl(new BMap.NavigationControl());    
+    map.addControl(new BMap.ScaleControl());    
+    map.addControl(new BMap.OverviewMapControl());    
+    map.addControl(new BMap.MapTypeControl());    
     var marker = new BMap.Marker(point);        // 创建标注    
     map.addOverlay(marker);
   }
